dbcd230dbf518bff5bcb8b8059b274f0056444ed
wild-breeze <trygugubest@gmail.com>
2025-10-22 09:55:14 +0800

    hello texture

---
+++ shader/shader.vert
@@ -0,0 +1,9 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+out vec4 ourColor; // 为片段着色器指定一个颜色输出
+void main() {
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   ourColor = vec4(aColor, 1.0); // 将ourColor设置为我们从顶点数据那里得到的输入颜色
+   // vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // 把输出变量设置为暗红色
+}
--- src/hello_texture.cpp
+++ src/hello_texture.cpp
@@ -4,7 +4,11 @@
 #include <iostream>
 #include <ostream>
 
+#include "stb_image.h"
 #include "shader_s.h"
+
+// stores how much we're seeing of either texture
+float mixValue = 0.2f;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -13,6 +17,23 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
+        // glfwSetWindowShouldClose(window, true);
+        mixValue += 0.001f;
+        if (mixValue > 1.0f) {
+            mixValue = 1.0f;
+        }
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
+        // glfwSetWindowShouldClose(window, true);
+        mixValue -= 0.001f;
+        if (mixValue < 0.0f) {
+            mixValue = 0.0f;
+        }
     }
 }
 
@@ -37,16 +58,17 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    const auto ourShader = Shader("../shader/shader.vert", "../shader/shader.fs");
+    const auto ourShader = Shader("../shader/texture.vert", "../shader/texture.frag");
 
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     constexpr float vertices[] = {
-        // 位置              // 颜色
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // 右下
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 左下
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // 顶部
+        // ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
     };
 
     constexpr GLubyte indices[] = {
@@ -68,11 +90,14 @@ int main(int argc, char *argv[]) {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // 位置属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
     // 颜色属性
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // 纹理坐标属性
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -81,20 +106,50 @@ int main(int argc, char *argv[]) {
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0);
 
-    // constexpr GLubyte indices2[] = {
-    //     0, 1, 2, // first triangle
-    //     1, 2, 3 // second triangle
-    // };
-    //
-    // unsigned int EBO2 ;
-    // glGenBuffers(1, &EBO2);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices2), indices2, GL_STATIC_DRAW);
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 
-    // uncomment this call to draw in wireframe polygons.
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // 创建纹理
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // 为当前绑定的纹理对象设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+    // 加载并生成纹理
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../assets/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    unsigned int texture2;
+    glGenTextures(1, &texture2);
+    glActiveTexture(GL_TEXTURE2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    data = stbi_load("../assets/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use(); // 不要忘记在设置uniform变量之前激活着色器程序！
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 1); // 手动设置
+    ourShader.setInt("texture2", 2); // 或者使用着色器类设置
 
     while (!glfwWindowShouldClose(window)) {
         // 输入
@@ -107,6 +162,7 @@ int main(int argc, char *argv[]) {
 
         // 记得激活着色器
         ourShader.use();
+        ourShader.setFloat("mixValue", mixValue);
 
         // 更新uniform颜色
         // const float timeValue = glfwGetTime();
@@ -117,11 +173,13 @@ int main(int argc, char *argv[]) {
 
         // 绘制三角形
         glBindVertexArray(VAO);
+        // glBindTexture(GL_TEXTURE_2D, texture2);
+
         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time
         // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
 
         // 交换缓冲并查询IO事件
         glfwPollEvents();
